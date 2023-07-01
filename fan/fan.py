7e2b3f9c0073cdcd7a9a5ee2098f4b704fadcf6e
hagiape <g_chrisf@protonmail.com>
2023-07-01 19:54:10 +0800

    Update fan.py
    Default values for the instance variables were modified.

--- fan/fan.py
+++ fan/fan.py
@@ -12,7 +12,7 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
-    def __init__(self, speed = int(SLOW), radius = float(5), color = str('blue'), on = False):
+    def __init__(self, speed = SLOW, radius = 5.0, color = "blue", on = False):
         self.__speed = speed
         self.__radius = radius
         self.__color = color
