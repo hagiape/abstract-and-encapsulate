283c0396138ddfed68375963263816c11adee8af
hagiape <g_chrisf@protonmail.com>
2023-07-01 21:17:32 +0800

    Update try_pet.py
    Pet class was imported from pet module.

--- pet/try_pet.py
+++ pet/try_pet.py
@@ -2,3 +2,4 @@
 # prompts the user to enter the name, type, and age of his or her pet. 
 # This data should be stored as the object’s attributes. 
 # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+from pet import Pet
