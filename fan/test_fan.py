994aa37ebcfd18282e41a2d1d7cdfea487ba20a9
hagiape <g_chrisf@protonmail.com>
2023-07-01 20:39:37 +0800

    Update test_fan.py
    Instance methods were utilized for the first fan object.

--- fan/test_fan.py
+++ fan/test_fan.py
@@ -4,4 +4,16 @@ fan1 = Fan()
 fan2 = Fan()
 
 fan1.set_speed(Fan.FAST)
-print(fan1.get_speed())
+print("The fan speed is " + str(fan1.get_speed()))
+
+fan1.set_radius(10.0)
+print("The fan speed is " + str(fan1.get_radius()))
+
+fan1.set_color("yellow")
+print("The color of the fan is " + fan1.get_color())
+
+fan1.set_on(True)
+if fan1.get_on == True:
+    print("The fan is currently on.")
+else:
+    print("The fan is currently off.")
