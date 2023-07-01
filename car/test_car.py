c1000f9b028d182a2e81e8304983a24b56ad7e2c
hagiape <g_chrisf@protonmail.com>
2023-07-01 19:40:51 +0800

    Update test_car.py
    accelerate method were called 5 times to demonstrate.

--- car/test_car.py
+++ car/test_car.py
@@ -6,3 +6,18 @@
 from car import Car
 
 my_car = Car(2006, "Honda")
+
+my_car.accelerate()
+print(my_car.get_speed())
+
+my_car.accelerate()
+print(my_car.get_speed())
+
+my_car.accelerate()
+print(my_car.get_speed())
+
+my_car.accelerate()
+print(my_car.get_speed())
+
+my_car.accelerate()
+print(my_car.get_speed())
