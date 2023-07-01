acd89efab2881f95987e2763bced0b04655a801a
hagiape <g_chrisf@protonmail.com>
2023-07-01 21:34:11 +0800

    Update try_pet.py
    Minor style changes (space was added for results).

--- pet/try_pet.py
+++ pet/try_pet.py
@@ -15,5 +15,5 @@ user_pet.set_animal_type(pet_type)
 user_pet.set_age(pet_age)
 
 print("Your pet's name is " + user_pet.get_name())
-print("Your pet is a" + user_pet.get_animal_type())
+print("Your pet is a " + user_pet.get_animal_type())
 print("Your pet is " + str(user_pet.get_age()) + " years old.")
