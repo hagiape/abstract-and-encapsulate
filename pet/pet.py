b6962f82cf8385d3bd8081b872d6855188257336
hagiape <g_chrisf@protonmail.com>
2023-07-01 21:35:35 +0800

    Update pet.py
    set_name method was corrected to modify __name.

--- pet/pet.py
+++ pet/pet.py
@@ -4,7 +4,7 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
     def set_name(self, name):
-        self.__age = name
+        self.__name = name
     def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
     def set_age(self, age):
