986b2d2cf1f2cfb1e0c81d9331e7c73a0b3e47a2
hagiape <g_chrisf@protonmail.com>
2023-07-01 17:26:56 +0800

    Update car.py
    Comments were added to serrve as guide for coding.

---
+++ car/car.py
@@ -0,0 +1,16 @@
+# Write a class named Car that has the following data attributes:
+# • _ _year_model (for the car’s year model)
+# • _ _make (for the make of the car)
+# • _ _speed (for the car’s current speed)
+
+# The Car class should have an _ _init_ _ method that accepts the car’s year model and make as arguments. 
+# These values should be assigned to the object’s _ _year_model and _ _make data attributes. 
+# It should also assign 0 to the _ _speed data attribute.
+
+# The class should also have the following methods:
+# • accelerate()
+# The accelerate method should add 5 to the speed data attribute each time it is called.
+# • brake()
+# The brake method should subtract 5 from the speed data attribute each time it is called.
+# • get_speed()
+# The get_speed method should return the current speed.
