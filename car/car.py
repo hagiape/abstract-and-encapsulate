2e920fbf2357beca682f1933ab69e27df62c9118
hagiape <g_chrisf@protonmail.com>
2023-07-01 19:16:51 +0800

    Update car.py
    self.__speed is set to 0 as the default value.

--- car/car.py
+++ car/car.py
@@ -16,7 +16,7 @@
 # The get_speed method should return the current speed.
 
 class Car:
-    def __init__(self, year_model, make, speed):
+    def __init__(self, year_model, make, speed = 0):
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
