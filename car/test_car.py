b5813a497ccc33be7492fa8aebbbc0020d05b676
hagiape <g_chrisf@protonmail.com>
2023-07-01 19:24:24 +0800

    Update test_car.py
    Comments were added as guide for coding.

---
+++ car/test_car.py
@@ -0,0 +1,3 @@
+# Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. 
+# This data should be stored as the object’s attributes. 
+# Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
