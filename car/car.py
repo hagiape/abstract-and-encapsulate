4ea6691982116a6ee56c7eefe38173e03308d768
hagiape <g_chrisf@protonmail.com>
2023-07-01 19:12:18 +0800

    Add methods
    accelerate, brake, and get_speed method were added.

--- car/car.py
+++ car/car.py
@@ -20,3 +20,12 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
+    
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        self.__speed -= 5
+
+    def get_speed(self):
+        return self.__speed
