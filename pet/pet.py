ee537d501be90f285e7411db5d6be4cea591ea6b
hagiape <g_chrisf@protonmail.com>
2023-07-01 21:01:17 +0800

    Update pet.py
    Mutator methods were created for the instance variables.

--- pet/pet.py
+++ pet/pet.py
@@ -22,3 +22,9 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+    def set_name(self, name):
+        self.__age = name
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+    def set_age(self, age):
+        self.__age = age
