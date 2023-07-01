6949f31c51a5de5da4a3522513f7c81b6e37b595
hagiape <g_chrisf@protonmail.com>
2023-07-01 20:52:43 +0800

    Add comments
    Comments were added to serve as guide for code structure.

---
+++ pet/pet.py
@@ -0,0 +1,18 @@
+# Write a class named Pet, which should have the following data attributes:
+# • _ _name (for the name of a pet)
+# • _ _animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
+# • _ _age (for the pet’s age)
+
+# The Pet class should have an _ _init_ _ method that creates these attributes. It should also have the following methods:
+# • set_name()
+# This method assigns a value to the _ _name field.
+# • set_animal_type()
+# This method assigns a value to the _ _animal_type field.
+# • set_age()
+# This method assigns a value to the _ _age field.
+# • get_name()
+# This method returns the value of the _ _ name field.
+# • get_animal_type()
+# This method returns the value of the _ _animal_type field.
+# • get_age()
+# This method returns the value of the _ _age field.
