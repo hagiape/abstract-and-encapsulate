fbde226c83ddd7321351c3e42b482dd2fb77a417
hagiape <g_chrisf@protonmail.com>
2023-07-01 19:30:15 +0800

    Update car.py
    Additional default values for year_model and make was added.

--- car/car.py
+++ car/car.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, year_model, make, speed = 0):
+    def __init__(self, year_model = 0, make = "N/A", speed = 0):
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
