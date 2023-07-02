48a1cb34b81f96103567972fc8e74a65158ca619
hagiape <g_chrisf@protonmail.com>
2023-07-02 17:53:00 +0800

    Update test_car.py
    get_make and get_year_model methods were utilized.

--- car/test_car.py
+++ car/test_car.py
@@ -2,6 +2,8 @@ from car import Car
 
 my_car = Car(2006, "Honda")
 
+print("My car is a " + my_car.get_make() + ", made in " + str(my_car.get_year_model()))
+
 my_car.accelerate()
 print("The car speed is currently "+ str(my_car.get_speed()))
 
