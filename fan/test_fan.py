7afc1ba03e5822918d359239adb3c15e6875da68
hagiape <g_chrisf@protonmail.com>
2023-07-08 21:59:23 +0800

    Update test_fan.py

--- fan/test_fan.py
+++ fan/test_fan.py
@@ -17,3 +17,18 @@ if fan1.get_on == True:
     print("The fan is currently on.")
 else:
     print("The fan is currently off.")
+
+fan2.set_speed(Fan.FAST)
+print("The speed of the fan is " + str(fan2.get_speed()))
+
+fan2.set_radius(10.0)
+print("The radius of the fan is " + str(fan2.get_radius()) + " units")
+
+fan2.set_color("yellow")
+print("The color of the fan is " + fan2.get_color())
+
+fan2.set_on(True)
+if fan2.get_on == True:
+    print("The fan is currently on.")
+else:
+    print("The fan is currently off.")
